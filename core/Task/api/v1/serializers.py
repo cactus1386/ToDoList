b3c5d461edea65795adb4860c798e180fa2a1b1f
cactus1386 <cactusinjast@gmail.com>
2024-03-01 15:58:58 -0800

    update models

--- core/Task/api/v1/serializers.py
+++ core/Task/api/v1/serializers.py
@@ -1,19 +1,19 @@
 from rest_framework import serializers
 from .models import *
 
-class TaskSerializer(serializers.ModelSerializer):
+class IsCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IsComplete
+        fields = ('id', 'is_complete')
+
+class TaskListSerializer(serializers.ModelSerializer):
+    is_complete = IsCompleteSerializer(source='check-set')
     class Meta:
         model = TaskList
         fields = ('id', 'task')
-        
-class CompleteCheckSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CompleteCheck
-        fields = ('id', 'is_complete')
 
 class DayListSerializer(serializers.ModelSerializer):
-    task = TaskSerializer(many=True, read_only=True,  source='task-set')
-    is_complete = CompleteCheckSerializer(many=True, read_only=True, source='complete-check')
+    task = TaskListSerializer(many=True, read_only=True, source='task-set')
     class Meta:
         model = DayList
-        fields = ('id', 'title', 'task', 'is_complete')
+        fields = ('id', 'title', 'task')
--- core/core/settings.py
+++ core/core/settings.py
@@ -31,7 +31,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
